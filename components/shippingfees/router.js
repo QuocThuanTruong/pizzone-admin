33745ddd550f19f35053b90ccc5c725d22547f7f
DESKTOP-HRO9T40\Admin <lenhattuan.it.hcmus@gmail.com>
2021-01-15 05:07:21 +0700

    update auth login

--- components/shippingfees/router.js
+++ components/shippingfees/router.js
@@ -5,13 +5,13 @@ const shippingController = require('./controller');
 const authController = require('../auth/controller')
 
 
-router.get('/', /*authController.isLogin,*/ shippingController.index);
+router.get('/', authController.isLogin, shippingController.index);
 router.post('/', authController.isLogin, shippingController.delete);
 
 router.get('/add', authController.isLogin, shippingController.add);
 router.post('/add', authController.isLogin, shippingController.confirmAdd);
 
-router.get('/update/:id', /*authController.isLogin,*/ shippingController.update);
-router.post('/update/:id', /*authController.isLogin,*/ shippingController.confirmUpdate);
+router.get('/update/:id', authController.isLogin, shippingController.update);
+router.post('/update/:id', authController.isLogin, shippingController.confirmUpdate);
 
 module.exports = router;
